485765ebe5761aaf59a782667d883b560972cb15
AtanasovCode <a_ljupce@yahoo.com>
2024-10-03 18:21:49 +0200

    initial file upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div className="w-full h-[100vh] bg-custom-black font-sans">
+      <h1 className="text-white">Base Project</h1>
+    </div>
+  );
+}
+
+export default App;
