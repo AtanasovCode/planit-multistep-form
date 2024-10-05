c6197c686a72fc2f7e2667957965adee4cb78370
AtanasovCode <a_ljupce@yahoo.com>
2024-10-05 22:21:40 +0200

    made step tracker responsive

--- src/components/Step.jsx
+++ src/components/Step.jsx
@@ -11,15 +11,25 @@ const Step = ({
     const backgroundColor = step === number ? "bg-selected-item" : "bg-transparent";
 
     return (
-        <div className="flex items-center justify-center mx-2">
+        <div className="
+            flex items-center justify-center mx-2
+            lg:mx-0 lg:mb-6
+        ">
             <div className={`
                 rounded-full w-10 aspect-square flex items-center justify-center border-2 
                 border-text text-white font-bold ${backgroundColor}
                 transition-all duration-300 ease-in-out cursor-pointer
+                lg:mr-4
             `}
             onClick={() => changeStep(number)}
             >
                 {number}
+            </div>
+            <div className="hidden lg:flex lg:flex-col lg:items-start lg:justify-center uppercase">
+                <div className="text-gray-300 font-semibold text-xs">Step {number}</div>
+                <div className="text-lg text-white font-semibold">
+                    {text}
+                </div>
             </div>
         </div>
     );
--- src/components/StepTracker.jsx
+++ src/components/StepTracker.jsx
@@ -3,9 +3,15 @@ import Step from "./Step";
 
 const StepTracker = () => {
     return (
-        <div className="text-text flex items-start justify-center pt-8 lg:rounded-xl relative h-[25dvh] w-[100vw]
-        bg-sidebar-mobile lg:bg-sidebar-desktop bg-cover bg-center bg-no-repeat">
-            <div className="w-full flex items-center justify-center">
+        <div className="
+            text-text flex items-start justify-center pt-8 relative h-[25dvh] w-[100vw]
+            bg-sidebar-mobile bg-cover bg-center bg-no-repeat
+            lg:bg-sidebar-desktop lg:w-[33%] lg:h-[100%] lg:rounded-xl lg:px-5
+        ">
+            <div className="
+                w-full flex items-center justify-center
+                lg:flex-col lg:justify-start lg:items-start
+            ">
                 <Step number={1} text="Your Info" />
                 <Step number={2} text="Select Plan" />
                 <Step number={3} text="Add-Ons" />
