ad1c0d2fc7aff156a352280c3905a437a70ff3b5
AtanasovCode <a_ljupce@yahoo.com>
2024-10-07 14:56:44 +0200

    implemented reusable modal selection in StepTwo component

--- src/steps/StepTwo.jsx
+++ src/steps/StepTwo.jsx
@@ -1,8 +1,19 @@
 import { useStore } from "../../useStore";
+
 import Heading from "../components/Heading";
+import ModelSelect from "../components/ModelSelect";
 
 
 const StepTwo = () => {
+
+    const {
+        pricing,
+        selectedModel,
+    } = useStore();
+
+    //getting the selected pricing model object
+    const pricingModel = pricing.find((item) => item.type === selectedModel);
+
     return (
         <div className="
             flex flex-col items-center justify-center text-text z-[99] 
@@ -12,6 +23,21 @@ const StepTwo = () => {
                 title="Select your plan"
                 subtitle="You have the option of monthly or yearly billing."
             />
+            <div className="
+                grid grid-cols-1 gap-2 justify-items-center w-full mt-4
+            ">
+                {
+                    pricingModel?.plans.map((plan) => {
+                        return (
+                            <ModelSelect
+                                key={plan.name}
+                                name={plan.name}
+                                price={plan.price}
+                            />
+                        );
+                    })
+                }
+            </div>
         </div>
     );
 }
