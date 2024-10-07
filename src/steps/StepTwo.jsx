66e12aa2eb4963b0aa51bc0a25bfe8b5cea7634e
AtanasovCode <a_ljupce@yahoo.com>
2024-10-07 14:24:15 +0200

    added reusable Heading component

--- src/steps/StepTwo.jsx
+++ src/steps/StepTwo.jsx
@@ -1,9 +1,17 @@
+import { useStore } from "../../useStore";
+import Heading from "../components/Heading";
 
 
 const StepTwo = () => {
     return (
-        <div className="text-text bg-card-background">
-            <h1>Step Two</h1>
+        <div className="
+            flex flex-col items-center justify-center text-text z-[99] 
+            lg:w-full
+        ">
+            <Heading
+                title="Select your plan"
+                subtitle="You have the option of monthly or yearly billing."
+            />
         </div>
     );
 }
