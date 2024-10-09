9002fa3be010cd4a7371183100c4410e016d0850
AtanasovCode <a_ljupce@yahoo.com>
2024-10-09 15:15:36 +0200

    improved mobile view

--- src/routes/Finish.jsx
+++ src/routes/Finish.jsx
@@ -1,42 +1,34 @@
-
 import checkmarkIcon from '../assets/icon-thank-you.svg';
 
 const Finish = () => {
     return (
-        <div className="
-            bg-background flex items-center justify-center text-text
-        ">
-            <div className="
-                flex flex-col items-center justify-start
-            ">
+        <div className="bg-background flex items-center justify-center text-text">
+            <div className="flex flex-col items-center justify-start">
                 <div className="
-                bg-sidebar-mobile bg-center bg-cover bg-no-repeat lg:bg-sidebar-desktop
-                h-[25dvh] w-[100vw] lg:w-[33%] lg:h-[100%] lg:rounded-xl
+                    w-[100vw] h-[25dvh] bg-sidebar-mobile bg-containt bg-center bg-no-repeat lg:bg-sidebar-desktop
+                    lg:w-[33%] lg:h-[100%]
+                ">{/* Background Image */}</div>
+                <div className="
+                    flex flex-col items-center justify-center z-[99] py-12 px-6 rounded-xl
+                    w-[95%] mt-[-60px] xs:w-[90%] sm:w-[85%] md:w-[70%] bg-card-background              
                 ">
-                    {/* Background Image */}
-                </div>
-                <div className="
-                bg-card-background w-[95%] rounded-xl mt-[-60px] py-12 px-6 z-[90]
-                flex flex-col items-center justify-center
-                xs:w-[90%] sm:w-[85%] md:w-[60%]
-            ">
-                    <div className="flex flex-col w-full items-center justify-center">
-                        <img
-                            src={checkmarkIcon}
-                            alt="checkmark icon"
-                            className="w-[33%]"
-                        />
-                        <div className="text-center font-bold text-3xl mt-4">
+                    <div className="flex flex-col items-center justify-center">
+                        <div className="flex items-center justify-center">
+                            <img
+                                src={checkmarkIcon}
+                                alt="checkmark illustration"
+                                className="w-10"
+                            />
+                        </div>
+                        <div className="font-bold text-3xl my-6">
                             Thank You!
                         </div>
                     </div>
-                    <div className="
-                    text-gray-300 text-base mt-6 text-center 
-                    xs:w-[70%] sm:w-[60%] md:w-[55%
-                ">
-                        Thanks for confirming your subscription! We hope you have
-                        fun using our platform. If you ever need support, please feel
-                        free to email us at support@loremgaming.com.
+                    <div className="text-gray-300 font-sm text-center">
+                        Thanks for confirming your subscription! We
+                        hope you have fun using our platform. If
+                        you ever need support, please feel free
+                        to email us at support@loremgaming.com.
                     </div>
                 </div>
             </div>
